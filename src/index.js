3f44e3b464aff77e5c09fca01fc3a4824a253b4c
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-19 14:59:05 +0300

    loaderEl.classList.remove
    loader не видаляється коли ставлю його у .finally

--- src/index.js
+++ src/index.js
@@ -3,31 +3,42 @@ import { creatMarkup, creatMarkupCat } from "./js/services/creatMurkup";
 // import creatMarkup from "./js/services/creatMurkup";
 import refs from "./js/refs";
 import SlimSelect from 'slim-select'
+import Notiflix from 'notiflix';
 
 const sel = new SlimSelect({
   select: '.breed-select'
 })
 
-const { selektEl, catInfo, loaderEl } = refs
+const { selektEl, catInfo, loaderEl,errorEl } = refs
 
 
 fetchBreeds()
     .then(element =>
         sel.setData(creatMarkup(element))
     )
-    .catch(console.log);
+  .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+  })
+  .finally(()=>loaderEl.classList.add('js-style'));
 
 selektEl.addEventListener('change', onSelectCat)
 
 function onSelectCat(e) {
   const id = e.target.value
   console.log(id);
-  
   loaderEl.classList.remove('js-style')
-  catInfo.innerHTML = "";
+  catInfo.innerHTML = " ";
   fetchCatByBreed(id).then(data =>
   // console.log(data))
-    catInfo.innerHTML = (creatMarkupCat(data)))
-    .catch(console.log)
+    catInfo.innerHTML = (creatMarkupCat(data))
+  )
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    })
+    .finally(()=>loaderEl.classList.add('js-style'))
   
 }
